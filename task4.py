6d6d34c1b4f3f845b84ac2fce739c985996bc674
Habiba Sadek <beebosalhin4@gmail.com>
2026-08-04 20:01:45 +0300

    task 4 upload

---
+++ task4.py
@@ -0,0 +1,43 @@
+import random
+import string
+lower = list(string.ascii_lowercase)
+upper = list(string.ascii_uppercase)
+special = ["!", "@", "#", "$", "%", "&", "*", "?", "_"]
+
+###### OPTION 1
+lenn = int(input("Enter password length: "))
+user = " "
+for letter in range(lenn):
+    user += random.choice([str(random.choice(range(10))), random.choice(special), random.choice(lower), random.choice(upper)])
+print(user)
+
+###### OPTION 2
+res = {"upperr": 0, "lowerr": 0, "speciall": 0, "numberr": 0}
+passs = input("Enter your password: ")
+fail = 0
+for i in passs:
+    try:
+        if int(i) in range(10):
+            res["numberr"] +=1
+    except:
+        if i in lower:
+            res["lowerr"] +=1
+        elif i in upper:
+            res["upperr"] += 1
+        elif i in special:
+            res["speciall"] += 1
+for j in res.values():
+    if j == 0:
+        fail +=1
+if len(passs) < 8:
+    fail += 1
+if fail >= 3:
+    print("Password Strength: Weak")
+elif fail == 2:
+    print("Password Strength: Medium")
+elif fail == 1:
+    print("Password Strength: Strong")
+elif fail == 0:
+    print("Password Strength: Very strong")
+
+    
