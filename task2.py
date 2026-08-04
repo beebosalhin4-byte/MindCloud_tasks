46fade641b79a3f7e4e13e1ea4d1c1b86037cef0
Habiba Sadek <beebosalhin4@gmail.com>
2026-08-04 16:26:27 +0300

    minor change

--- task2.py
+++ task2.py
@@ -2,6 +2,7 @@ import time
 minutess = int(input("enter the minutes: "))
 secondss = int(input("enter the seconds: "))
 timee= minutess*60 + secondss
+state = " "
 if timee<= 0 or secondss > 59:
     print("Invalid test duration.")
     sys.exit()
@@ -12,6 +13,16 @@ for second in range(timee,-1,-1):
     time.sleep(1)
     mins = second // 60
     secs = second % 60
-    print(f"{mins}:{secs}", end = "\r")
+    if second > 30:
+        state = "POWER ON"
+        print(f"{state} | Remaining: {mins}:{secs}", end = "\r")
+    elif second <= 30 and second > 10: 
+        state = "STABILIZING SYSTEM"
+        print(f"{state} | Remaining: {mins}:{secs}", end = "\r")
+    elif second <= 10 and second != 0:
+        state = "COOLDOWN PHASE | Do not touch"
+        print(f"{state} | Remaining: {mins}:{secs}", end = "\r")
+    elif second == 0:
+        print(f"{state} | Remaining: {mins}:{secs}", end = "\n")
 print("Power test completed successfully.")
 
